7b1e4e0ce9c3af97dd650706e544bbd4a097acf7
Stanislav <stazilastas@gmail.com>
2023-09-28 09:50:29 -0500

    ViewControllers 1.0

--- ViewControllersFun/ViewController.swift
+++ ViewControllersFun/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var pony = ""
+    @IBOutlet weak var textFieldOutlet: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func manualAction(_ sender: Any) {
+        print("Hi")
+        
+         pony = textFieldOutlet.text!
+        
+        performSegue(withIdentifier: "toRed", sender: self)
+        
+        
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // NVC (next view controller) giving access to the next view controller
+        let nvc = segue.destination as! ViewControllerStacks
+        
+        nvc.name = pony
+        
+    }
+    
 }
 
---
+++ ViewControllersFun/ViewControllerStacks.swift
@@ -0,0 +1,26 @@
+//
+//  ViewControllerStacks.swift
+//  ViewControllersFun
+//
+//  Created by STANISLAV STAJILA on 9/28/23.
+//
+
+import UIKit
+
+class ViewControllerStacks: UIViewController {
+
+    var name: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print(name)
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func buttonAction(_ sender: Any) {
+        view.backgroundColor = UIColor.yellow
+    }
+    
+}
